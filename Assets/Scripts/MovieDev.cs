89fb1705f2c323a1f12fa43c1db83428f336a472
TJ <jtorvald@gmail.com>
2015-11-10 04:05:37 +0100

    Made 2 classes to play movies. MoviePlayer attached to app-controller, has public object for movietexture. And MovieDev, just attach to object for movietexture. Also imported some possibly redundant, useless asset bundles while testing stuff. Vuforia Video should be deleted.

---
+++ Assets/Scripts/MovieDev.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Events;
+
+public class MovieDev:MonoBehaviour
+{
+	
+	MovieTexture mt;
+	RectTransform rt;
+	Vector2 origPos;
+	private WWW www;
+	
+	void Awake()
+	{
+		rt = GetComponent<RectTransform>();
+		origPos = rt.anchoredPosition;
+
+		www = new WWW("file://" + Application.streamingAssetsPath + "/Converted.ogg");
+
+		while (!www.isDone) {
+			
+			//yield www;
+			
+			Debug.Log ("Done Downloading Texture");
+			
+			if (www.isDone) {
+				break; //if done downloading image break loop
+			}
+		}
+
+		mt = www.movie;
+
+		RawImage rim = GetComponent<RawImage>();
+		rim.texture = mt;
+		
+		
+
+		mt = (MovieTexture)rim.mainTexture;
+	}
+	
+	void Update()
+	{
+		
+		//if (Input.GetButtonDown("Jump"))
+		//{
+			if (!mt.isPlaying)
+			{
+				mt.Play();
+			}
+			//else
+			//{
+				//mt.Stop();
+				//mt.Play();
+			//}
+		//}
+		
+		if (Input.GetKeyDown(KeyCode.W))
+			rt.Translate(0f, 1f, 0f);
+		if (Input.GetKeyDown(KeyCode.X))
+			rt.Translate(0f, -1f, 0f);
+		
+		if (Input.GetKeyDown(KeyCode.A))
+			rt.Translate(-1f, 0f, 0f);
+		if (Input.GetKeyDown(KeyCode.D))
+			rt.Translate(1f, 0f, 0f);
+		
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			rt.anchoredPosition = origPos;
+			rt.eulerAngles = Vector3.zero;
+			rt.localScale = new Vector3( 1f, 1f, 1f );
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Q))
+			rt.Rotate(0f, 0f, .25f);
+		if (Input.GetKeyDown(KeyCode.E))
+			rt.Rotate(0f, 0f, -.25f);
+		
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			float scaleNow = rt.localScale.x;
+			if (scaleNow < 0.5f ) return;
+			scaleNow = scaleNow - 0.01f;
+			rt.localScale = new Vector3( scaleNow, scaleNow, 1f );
+		}
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			float scaleNow = rt.localScale.x;
+			if (scaleNow > 1.5f ) return;
+			scaleNow = scaleNow + 0.01f;
+			rt.localScale = new Vector3( scaleNow, scaleNow, 1f );
+		}
+		
+	}
+}
--- Assets/Scripts/MoviePlayer.cs
+++ Assets/Scripts/MoviePlayer.cs
@@ -1,20 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MoviePlayer : MonoBehaviour {
 
+	private WWW www;
+	private bool done=false;
+	private MovieTexture mt;
+
+	public RawImage m_vidImage;
+
 	// Use this for initialization
 	void Start () {
-	
+		www = new WWW("file://" + Application.streamingAssetsPath + "/Converted.ogg");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (www.isDone==false && string.IsNullOrEmpty(www.error)==true)
+		{
+			Debug.Log("Progress: " + www.progress);
+		}
+		
+		if (done==true)
+			return;
+		
+		mt = www.movie;       
+		if (www.isDone && string.IsNullOrEmpty(www.error)==false)
+		{
+			Debug.Log(www.error);
+			done=true;
+		}
+		
+		if (mt != null && mt.isReadyToPlay==true && mt.isPlaying == false)
+		{
+			/*
+			audio.clip = mt.audioClip;
+			audio.Play();
+			
+			renderer.material.mainTexture = mt;
+			mt.Play();
+			done=true;
+			*/
+		}
 	
 	}
 
 	public void PlayMovie () {
-		Handheld.PlayFullScreenMovie ("/IMG_0342.mov", Color.black, FullScreenMovieControlMode.Full);
+
+		//Handheld.PlayFullScreenMovie (www, Color.black, FullScreenMovieControlMode.Full);
+	}
+
+	public void PlayUIMovie() {
+		//mt.Play();
+		m_vidImage.texture = mt;
+		mt.Play();
+		//m_vidImage.mainTexture.Play();
+
+		//function Start(){ GetComponent(RawImage).mainTexture.Play(); }
 	}
 
 }
